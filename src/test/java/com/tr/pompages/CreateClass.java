edf9d7c6d1f6882b9e9a2218e190a024e8584e39
Santdc <santoshdev46@gmail.com>
2016-05-08 14:30:39 +0530

    UpdatedClassfile

--- src/test/java/com/tr/pompages/CreateClass.java
+++ src/test/java/com/tr/pompages/CreateClass.java
@@ -1,5 +1,16 @@
 package com.tr.pompages;
 
-public class CreateClass {
+import org.openqa.selenium.WebDriver;
 
+public class CreateClass {
+	
+	
+	WebDriver  driver;
+	
+	public CreateClass(WebDriver driver) {
+		this.driver =driver;
+		
+	}
 }
+
+
